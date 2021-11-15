6ed590af707068f99096f675113ae463da289472
Tominous <18189490+Tominous@users.noreply.github.com>
2021-11-15 16:17:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name='social-analyzer',
+    name='social-analyser',
     author='QeeqBox',
     author_email='gigaqeeq@gmail.com',
     description="API, CLI & Web App for analyzing & finding a person's profile across 300+ social media websites (Detections are updated regularly)",
